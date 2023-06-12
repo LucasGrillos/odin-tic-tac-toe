358f028e4774b9ed4aac31a5903888fd1053b38d
Lucas Grillos <LucasGrillos@gmail.com>
2023-06-11 22:46:08 -0700

    add function to render marks to display from gameboard

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const X = 'X';
+
+
 const Gameboard = (() => {
     let board = [
-        '', '', '',
-        '', '', '',
-        '', '', ''
+        '', 'x', 'x',
+        '', 'x', 'o',
+        'x', 'o', 'o'
     ]
 
     const getBoard = () => {
@@ -21,6 +24,19 @@ const Gameplay = (() => {
 
 const DisplayController = (() => {
 
+    const renderBoard = () => {
+        let board = Gameboard.getBoard();
+        for (var i=0; i<board.length; i++) {
+            let mark = document.querySelector(`#tile${String(i+1)} > div`);
+            if(board[i]) {
+                mark.classList.add(board[i]);
+            }
+        }
+    }
+
+    return({
+        renderBoard
+    })
 })();
 
 const Player = (mark) => {
