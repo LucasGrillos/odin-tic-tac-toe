d96145e2b087491d6ae119c2f237e54cbb713cbc
Lucas Grillos <LucasGrillos@gmail.com>
2023-06-15 00:55:04 -0700

    fix bug adding multiple mark divs to tile element
    add array of win state arrays
    
    add initial win function logic

--- script.js
+++ script.js
@@ -44,6 +44,19 @@ const Gameplay = (() => {
     let player2 = Player(Y);
     let turn = 1;
 
+    const winConditions = [
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+
+        [0,4,8],
+        [2,4,6]
+    ];
+
     const createPlayers = (player, mark) => {
 
     }
@@ -51,13 +64,27 @@ const Gameplay = (() => {
     const getCurrentPlayer = () => {
         return (turn % 2 == 0 ? player2.getMark() : player1.getMark() )
         // returns 'x' or 'o'
+    }
+
+    const checkWin = (curPlayer) => {
+        let board = Gameboard.getBoard();
+        let curIndices = [];
+        for (var i=0;i<board.length;i++) {
+            if (board[i] == curPlayer) {
+                curIndices.push(board[i]);
+            }
+        }
+
+        console.log(curIndices);
     } 
 
     const clickTile = (tileNumber) => {
         if(!Gameboard.checkTileFull(tileNumber)) {
-            Gameboard.addMark(tileNumber, getCurrentPlayer())
+            let curPlayer = getCurrentPlayer();
+            Gameboard.addMark(tileNumber, curPlayer);
             turn++;
             DisplayController.renderBoard();
+            checkWin(curPlayer);
         }
 
         else {
@@ -76,8 +103,9 @@ const DisplayController = (() => {
     const renderBoard = () => {
         let board = Gameboard.getBoard();
         for(var i=0; i<board.length;i++) {
-            if(board[i]) {
-                let tile = document.querySelector(`#tile${i}`);
+            let tile = document.querySelector(`#tile${i}`);
+            
+            if(board[i] && !tile.firstChild) {
                 let markDiv = document.createElement('div');
                 markDiv.classList.add("mark", String(board[i]));
                 tile.appendChild(markDiv);
